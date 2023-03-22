a63a57a89e20b19ddb7b7971a74f9eda656e757e
kiryong159 <kiryong159@naver.com>
2023-03-22 17:28:42 +0900

    1.2

---
+++ src/components/Router.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+  return (
+    <Router>
+      <Routes>
+        {loggedIn ? (
+          <>
+            <Route path="/" matchExactly element={<Home />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" matchExactly element={<Auth />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
