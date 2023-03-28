3f3c150cd586801679272d1a68b32474b8545606
kiryong159 <kiryong159@naver.com>
2023-03-28 16:06:00 +0900

    deploy 2

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import App from "./components/App";
 import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter basename="/">
+    <App />
+  </BrowserRouter>
+);
